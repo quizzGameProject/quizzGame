2c63e4255be67b99281b56506bd36983a974fd73
YOUR_GUTHUB_NAME <brahamselim09@gmail.com>
2024-01-12 20:42:05 +0100

    working on html files & Local Storage

---
+++ doc/JS/signlogin.js
@@ -0,0 +1,36 @@
+function SignUp() {
+  var firstName = document.getElementById("firstName").value;
+  var lastName = document.getElementById("lastName").value;
+  var username = document.getElementById("username").value;
+  var email = document.getElementById("email").value;
+  var password = document.getElementById("password").value;
+
+  var user = {
+    firstName: firstName,
+    lastName: lastName,
+    username: username,
+    email: email,
+    password: password,
+  };
+
+  console.log(user);
+
+  localStorage.setItem("user", JSON.stringify(user));
+}
+
+function signIn() {
+  var enteredEmail = document.getElementById("email").value;
+  var enteredPassword = document.getElementById("psw").value;
+
+  var storedUser = JSON.parse(localStorage.getItem("user"));
+
+  if (
+    storedUser &&
+    enteredEmail === storedUser.email &&
+    enteredPassword === storedUser.password
+  ) {
+    window.location.href = "../html/index.html";
+  } else {
+    console.log("Sign-in failed.");
+  }
+}
